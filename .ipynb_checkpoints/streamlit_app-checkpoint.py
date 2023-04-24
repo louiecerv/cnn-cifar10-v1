3855b7fc5111d4e833cdb27a519ce68b0770bda3
unknown <louiecervantes@wvsu.edu.ph>
2023-04-24 12:37:09 +0800

    save changes

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -100,35 +100,46 @@ def app():
         
         if st.button('Run the Neural Network'):
             
-            # Normalize pixel values to be between 0 and 1
-            train_images, test_images = train_images / 255.0, test_images / 255.0
-            st.write(train_images[0])
-            st.write(test_images[0])
-            
+            # Normalize pixel values between 0 and 1
+            x_train = x_train.astype("float32") / 255.0
+            x_test = x_test.astype("float32") / 255.0
+
+            # Convert class labels to one-hot encoded vectors
+            num_classes = 10
+            y_train = keras.utils.to_categorical(y_train, num_classes)
+            y_test = keras.utils.to_categorical(y_test, num_classes)
+
             # Define the CNN architecture
-            model = models.Sequential()
-            model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-            model.add(layers.MaxPooling2D((2, 2)))
-            model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-            model.add(layers.MaxPooling2D((2, 2)))
-            model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-            model.add(layers.Flatten())
-            model.add(layers.Dense(64, activation='relu'))
-            model.add(layers.Dense(10))
+            model = keras.Sequential(
+                [
+                    layers.Conv2D(32, (3, 3), activation="relu", input_shape=(32, 32, 3)),
+                    layers.MaxPooling2D(pool_size=(2, 2)),
+                    layers.Conv2D(64, (3, 3), activation="relu"),
+                    layers.MaxPooling2D(pool_size=(2, 2)),
+                    layers.Conv2D(128, (3, 3), activation="relu"),
+                    layers.Flatten(),
+                    layers.Dense(128, activation="relu"),
+                    layers.Dense(num_classes, activation="softmax"),
+                ]
+            )
 
             # Compile the model
-            model.compile(optimizer='adam',
-                          loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                          metrics=['accuracy'])
+            model.compile(
+                loss="categorical_crossentropy",
+                optimizer="adam",
+                metrics=["accuracy"],
+            )
 
             # Train the model
-            history = model.fit(train_images, train_labels, epochs=2, 
-                                validation_data=(test_images, test_labels), callbacks=[CustomCallback()])
+            batch_size = 64
+            epochs = 10
+            model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test), callbacks=[CustomCallback])
 
-            # Evaluate the model
-            test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
-            print("Test accuracy:", test_acc)
-            
+            # Evaluate the model on the test set
+            score = model.evaluate(x_test, y_test, verbose=0)
+            print("Test loss:", score[0])
+            print("Test accuracy:", score[1])
+
     st.write('Conclusion')        
    
 #run the app
