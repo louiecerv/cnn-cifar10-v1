a8c5987e98c937907005efcbc3c4fc05a3ffde27
unknown <louiecervantes@wvsu.edu.ph>
2023-04-24 10:49:12 +0800

    save changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,14 +15,29 @@ def app():
     st.subheader('by Louie F. Cervantes M.Eng., WVSU College of ICT')
     st.subheader('The Convolutional Neural Network')
 
-    st.write('A convolutional neural network (CNN) is a type of artificial neural network that is commonly used for image recognition and computer vision tasks. The key idea behind a CNN is to use filters, or kernels, that can be convolved with the input data to extract relevant features.')
+    st.write('A convolutional neural network (CNN) is a type of artificial neural network \
+    that is commonly used for image recognition and computer vision tasks. The key idea \
+    behind a CNN is to use filters, or kernels, that can be convolved with the input data \
+    to extract relevant features.')
     
- st.write('A CNN typically consists of multiple layers, including convolutional layers, pooling layers, and fully connected layers. The convolutional layers are responsible for applying the filters to the input data, which helps to identify features such as edges, corners, and textures. The pooling layers are used to downsample the output of the convolutional layers, reducing the spatial dimensions of the data and helping to make the network more efficient. The fully connected layers are used to perform the final classification or regression task, taking the output of the previous layers and producing a final output.')
+     st.write('A CNN typically consists of multiple layers, including convolutional layers, \
+     pooling layers, and fully connected layers. The convolutional layers are responsible \
+     for applying the filters to the input data, which helps to identify features such as edges, \
+     corners, and textures. The pooling layers are used to downsample the output of the \
+     convolutional layers, reducing the spatial dimensions of the data and helping to make \
+     the network more efficient. The fully connected layers are used to perform the final \
+     classification or regression task, taking the output of the previous layers and \
+     producing a final output.')
 
- st.write('During the training process, the weights of the filters and the parameters of the fully connected layers are adjusted through backpropagation, using a loss function such as cross-entropy or mean squared error. This allows the network to learn to recognize patterns and features in the input data, and to make accurate predictions on new data.')
+     st.write('During the training process, the weights of the filters and the parameters \
+     of the fully connected layers are adjusted through backpropagation, using a loss function \
+     such as cross-entropy or mean squared error. This allows the network to learn to \
+     recognize patterns and features in the input data, and to make accurate predictions on new data.')
  
     st.subheader('The CIFAR10-Dataset')
-    st.write('CIFAR-10 is a popular image classification dataset that consists of 60,000 32x32 color images in 10 classes, with 6,000 images per class. The dataset is divided into 50,000 training images and 10,000 testing images.')
+    st.write('CIFAR-10 is a popular image classification dataset that consists of 60,000 \
+    32x32 color images in 10 classes, with 6,000 images per class. The dataset is divided \
+    into 50,000 training images and 10,000 testing images.')
     st.write('The 10 classes in the CIFAR-10 dataset are: \
     \n1. Airplane \
     \n2. Automobile \
@@ -35,7 +50,9 @@ def app():
      \n9. Ship \
      \n10. Truck')
     
-    st.write('The images in the CIFAR-10 dataset are low-resolution (32x32) and have a color depth of 3 (RGB). The dataset was collected by researchers at the Canadian Institute for Advanced Research (CIFAR) and is commonly used as a benchmark for image classification tasks.')
+    st.write('The images in the CIFAR-10 dataset are low-resolution (32x32) and have a color \
+    depth of 3 (RGB). The dataset was collected by researchers at the Canadian Institute for \
+    Advanced Research (CIFAR) and is commonly used as a benchmark for image classification tasks.')
     
     # Load the CIFAR-10 dataset
     (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
@@ -58,7 +75,7 @@ def app():
                 plt.xlabel(class_names[train_labels[i][0]])
             st.pyplot(fig)
             
-        if st.button('Run the Neural Network'):=
+        if st.button('Run the Neural Network'):
             #set the number of hidden layers
             neurons = st.slider('No. of neurons in the hidden layer', 5, 15, 10)
             #set the number or iterations
